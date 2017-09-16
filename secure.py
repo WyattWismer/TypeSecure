617f38c1f7ac33315314d3e223dbd2a4564a1997
WyattWismer <crazy123frog@gmail.com>
2017-09-16 11:09:35 -0400

    collect skew

--- secure.py
+++ secure.py
@@ -3,6 +3,7 @@ import pyHook
 import pythoncom
 import time
 
+
 #Load identity
 name = raw_input("User identity to be loaded: ")
 
@@ -13,7 +14,7 @@ with open(name+'.pkl','rb') as pkl:
 #Constants
 KEY_TIMEOUT = 5 #Seconds
 OLD_BIAS = 0.9
-ERROR_THRESHOLD = 0.175
+ERROR_THRESHOLD = 0.18
 
 
 #Maintain current word
@@ -23,12 +24,13 @@ last_time = time.time()
 
 #Detecting error
 error_total = 0
+error_skew_total = 0
 words = 0
 
 
 #Handle typing detection
 def OnKeyboardEvent(event):
-    global last_time, word, timing, name, words, error_total
+    global last_time, word, timing, name, words, error_total, error_skew_total
     #check if letter
     if (event.Ascii >= 65 and event.Ascii <= 90) or (event.Ascii >= 97 and event.Ascii <= 122):
         if time.time() - last_time < KEY_TIMEOUT:
@@ -57,9 +59,14 @@ def OnKeyboardEvent(event):
             print len(timing)
             #iden[word] = [ OLD_BIAS * iden[word][i] + (1 - OLD_BIAS) * timing[i] for i in range(len(timing))]
             words += 1
+            word_time = sum(timing)
             error_total += sum([abs(timing[i]-iden[word][i]) for i in range(len(timing))])
+            if len(timing) > 1:
+                error_skew_total += abs( (max(timing)-min(timing)) - (max(iden[word])-min(iden[word])) )
             if words > 30 and error_total/float(words) > ERROR_THRESHOLD:
-                print "You are a suspicious person", error_total/float(words)
+                print "You are a suspicious person"
+                print "Regular Error", error_total/float(words)
+                print "Skew", error_skew_total/float(words)
         #print word, iden[word]
     elif word == "savetraining":
         with open(name+'.pkl','wb') as output:
